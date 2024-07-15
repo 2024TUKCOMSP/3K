4093fb128bf9a1e60c26f67414a02d63d0de862c
KimSeongRyeol <kab6440@naver.com>
2024-07-15 21:18:54 +0900

    Add signUp function to LoginActivity

--- app/src/main/java/com/example/chatapplication/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapplication/SignUpActivity.kt
@@ -1,17 +1,45 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.chatapplication.databinding.ActivitySignUpBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignUpBinding
+    lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        mAuth = Firebase.auth
+
+        binding.signUpBtn.setOnClickListener {
+            val email = binding.emailEdit.text.toString().trim()
+            val password = binding.passwordEdit.text.toString().trim()
+
+            singUp(email, password)
+        }
+    }
+
+    private fun singUp(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
