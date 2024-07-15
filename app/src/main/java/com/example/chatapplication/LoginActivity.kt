c6310155f33b5fcd90175e017afe29b111a173f0
KimSeongRyeol <kab6440@naver.com>
2024-07-15 20:17:26 +0900

    Design login UI

--- app/src/main/java/com/example/chatapplication/LoginActivity.kt
+++ app/src/main/java/com/example/chatapplication/LoginActivity.kt
@@ -5,15 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.chatapplication.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
+import com.example.chatapplication.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 }
