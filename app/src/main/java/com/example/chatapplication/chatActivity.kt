71919ce99c16e8ba61cb8180589d39cdc9902651
Jinwoo Kim <87019634+Vacal4123@users.noreply.github.com>
2024-07-17 01:27:36 +0900

    chat UI base 업데이트

---
+++ app/src/main/java/com/example/chatapplication/chatActivity.kt
@@ -0,0 +1,17 @@
+package com.example.chatapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.chatapplication.databinding.ActivityChatBinding
+
+class chatActivity : AppCompatActivity() {
+    lateinit var binding:ActivityChatBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
