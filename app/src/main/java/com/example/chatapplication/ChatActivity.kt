a8ea9f523363a1fbabe0eb1663904a379bf1f3c4
KimSeongRyeol <kab6440@naver.com>
2024-07-18 12:52:39 +0900

    Rename class names to uppercase

--- app/src/main/java/com/example/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -1,17 +1,13 @@
 package com.example.chatapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapplication.databinding.ActivityChatBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class chatActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
     private lateinit var receiverName: String
     private lateinit var receiverUid: String
     lateinit var binding:ActivityChatBinding
