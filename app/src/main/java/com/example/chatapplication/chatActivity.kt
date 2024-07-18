5178e41efa5dc717552d088ad48d03f0d257f8c4
KimSeongRyeol <kab6440@naver.com>
2024-07-18 12:51:10 +0900

    Implement sendMessage function to enable message sending

--- app/src/main/java/com/example/chatapplication/chatActivity.kt
+++ app/src/main/java/com/example/chatapplication/chatActivity.kt
@@ -7,15 +7,46 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapplication.databinding.ActivityChatBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class chatActivity : AppCompatActivity() {
     private lateinit var receiverName: String
-    private lateinit var receiveruid: String
+    private lateinit var receiverUid: String
     lateinit var binding:ActivityChatBinding
+
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mDbRef: DatabaseReference
+
+    private lateinit var receiverRoom: String
+    private lateinit var senderRoom: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().reference
+
+        val senderUid = mAuth.currentUser?.uid
+
+        senderRoom = receiverUid + senderUid
+
+        receiverRoom = senderUid + receiverUid
+
+        supportActionBar?.title = receiverName
+
+        binding.chatActivityButton.setOnClickListener {
+            val message = binding.chatActivityEdittext.text.toString()
+            val messageObject = Message(message, senderUid)
+
+            mDbRef.child("chats").child(senderRoom).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom).child("message").push()
+                        .setValue(messageObject)
+                }
+        }
     }
 }
