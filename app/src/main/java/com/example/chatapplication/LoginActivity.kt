c0f001e1a2acc9c6405695a2b06af9b8d2292764
KimSeongRyeol <kab6440@naver.com>
2024-07-20 23:54:16 +0900

    Implement Google Login functionality

--- app/src/main/java/com/example/chatapplication/LoginActivity.kt
+++ app/src/main/java/com/example/chatapplication/LoginActivity.kt
@@ -2,11 +2,20 @@ package com.example.chatapplication
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.media.session.MediaSession.Token
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatapplication.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -14,10 +23,16 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
+
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     lateinit var mAuth: FirebaseAuth
     lateinit var sharedPref: SharedPreferences
+
+    var RC_SIGN_IN = 9000
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -26,7 +41,9 @@ class LoginActivity : AppCompatActivity() {
         mAuth = Firebase.auth
         sharedPref = getSharedPreferences("com.example.chatapplication_preferences", MODE_PRIVATE)
 
-        binding.signUpBtn.setOnClickListener{
+
+
+        binding.signUpBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
@@ -35,12 +52,23 @@ class LoginActivity : AppCompatActivity() {
             val email = binding.emailEdit.text.toString()
             val password = binding.passwordEdit.text.toString()
 
-            if(!email.isEmpty() || !password.isEmpty())
+            if (!email.isEmpty() || !password.isEmpty())
                 login(email, password)
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.my_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.googleSignUpBtn.setOnClickListener {
+            signIn()
         }
     }
 
-    private fun login(email: String, password: String){
+    private fun login(email: String, password: String) {
         mAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -49,18 +77,22 @@ class LoginActivity : AppCompatActivity() {
                         val uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
                         val mDbRef = Firebase.database.reference
                         putString("uid", uid)
-                        mDbRef.child("user").child(uid).child("font").addValueEventListener(object : ValueEventListener {
-                            override fun onDataChange(snapshot: DataSnapshot) {
-                                for(shot in snapshot.children) {
-                                    putString(shot.key, shot.value.toString())
+                        mDbRef.child("user").child(uid).child("font")
+                            .addValueEventListener(object : ValueEventListener {
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    for (shot in snapshot.children) {
+                                        putString(shot.key, shot.value.toString())
+                                    }
+                                    commit()
                                 }
-                                commit()
-                            }
-                            override fun onCancelled(error: DatabaseError) { }
-                        })
+
+                                override fun onCancelled(error: DatabaseError) {}
+                            })
                     }
-                    val intent:Intent = Intent(this@LoginActivity,
-                        ChatListActivity::class.java)
+                    val intent: Intent = Intent(
+                        this@LoginActivity,
+                        ChatListActivity::class.java
+                    )
                     startActivity(intent)
                     Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
                     finish()
@@ -70,4 +102,48 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
     }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent //구글로그인 페이지로 가는 인텐트 객체
+        startActivityForResult(signInIntent, RC_SIGN_IN) //Google Sign In flow 시작
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(
+                    ApiException::class.java
+                )
+                Log.d("TAG", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!, account)
+            } catch (e: ApiException) {
+                Log.d("TAG", "로그인 실패", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String, account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this@LoginActivity, "구글 로그인 성공", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(
+                        applicationContext,
+                        ChatListActivity::class.java
+                    )
+                    intent.putExtra("UserName", account.displayName)
+                    intent.putExtra("PhotoUrl", account.photoUrl)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LoginActivity, "구글 로그인 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
+
