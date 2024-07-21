3eaf38432460406dcb4dd404660e7eb20108934b
KimSeongRyeol <kab6440@naver.com>
2024-07-21 20:10:57 +0900

    Implement Firebase authentication using Google credentials

--- app/src/main/java/com/example/chatapplication/LoginActivity.kt
+++ app/src/main/java/com/example/chatapplication/LoginActivity.kt
@@ -15,6 +15,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
