7c6713ebbd099cf445f4dfc88742bd2609d4ce15
KimSeongRyeol <kab6440@naver.com>
2024-07-22 15:28:05 +0900

    Implemented social login using Kakao API

--- app/src/main/java/com/example/chatapplication/LoginActivity.kt
+++ app/src/main/java/com/example/chatapplication/LoginActivity.kt
@@ -6,7 +6,7 @@ import android.media.session.MediaSession.Token
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
+import com.kakao.sdk.common.util.Utility
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatapplication.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -23,6 +23,11 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 
 
 class LoginActivity : AppCompatActivity() {
@@ -33,7 +38,6 @@ class LoginActivity : AppCompatActivity() {
     var RC_SIGN_IN = 9000
 
     private lateinit var mGoogleSignInClient: GoogleSignInClient
-    private lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,8 +46,6 @@ class LoginActivity : AppCompatActivity() {
 
         mAuth = Firebase.auth
         sharedPref = getSharedPreferences("com.example.chatapplication_preferences", MODE_PRIVATE)
-
-
 
         binding.signUpBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
@@ -63,10 +65,16 @@ class LoginActivity : AppCompatActivity() {
             .requestEmail()
             .build()
 
+        KakaoSdk.init(this, R.string.kakao_my_web.toString())
+
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
         binding.googleSignUpBtn.setOnClickListener {
             signIn()
+        }
+
+        binding.kakaoSignUpBtn.setOnClickListener {
+            kakaoLogin()
         }
     }
 
@@ -162,6 +170,40 @@ class LoginActivity : AppCompatActivity() {
                     Toast.makeText(this@LoginActivity, "구글 로그인 실패", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun kakaoLogin() {
+        // 카카오계정으로 로그인 공통 callback 구성
+// 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e("TAG", "카카오계정으로 로그인 실패", error)
+            } else if (token != null) {
+                Log.i("TAG", "카카오계정으로 로그인 성공 ${token.accessToken}")
+            }
+        }
+
+// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                if (error != null) {
+                    Log.e("TAG", "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                } else if (token != null) {
+                    Log.i("TAG", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+        }
     }
 }
 
