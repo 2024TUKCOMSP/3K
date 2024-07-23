367c4684938366c6c52cc84c77d74c7d3926e350
Kim Jinwoo <vacal412@naver.com>
2024-07-24 01:48:33 +0900

    채팅방 리스트 타임스탬프 업데이트

--- app/src/main/java/com/example/chatapplication/Message.kt
+++ app/src/main/java/com/example/chatapplication/Message.kt
@@ -1,12 +1,12 @@
 package com.example.chatapplication
 
 import com.google.android.gms.common.internal.Objects
-import com.google.firebase.Timestamp
+import com.google.firebase.database.ServerValue
 
 data class Message(
     val message: String?,
     var sendId: String?,
-    var timestamp: String?
+    val timestamp: Any
 ){
-    constructor(): this("", "", "")
+    constructor(): this("", "", ServerValue.TIMESTAMP)
 }
